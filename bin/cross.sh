de75b04653c600c219da17b0f2329d9ba9b49777
mesut <mesutdogansoy@gmail.com>
2025-05-21 10:26:22 +0000

    fix arm64 script

--- bin/cross.sh
+++ bin/cross.sh
@@ -1,15 +1,25 @@
 dir=$(dirname $0)
 
 if [ ! -d "$1" ]; then
- echo "provide toolchain dir"
+ echo "provide host toolchain dir"
+ exit
+fi
+
+if [ ! -d "$2" ]; then
+ echo "provide target toolchain dir"
  exit
 fi
 
 toolchain=$1
+toolchain_target=$2
 compiler="$toolchain/bin/x"
 build=$dir/../build
 name="x_arm64"
 out_dir=$build/${name}_out
+
+sudo dpkg --add-architecture arm64
+sudo apt update
+sudo apt install -y g++-13-aarch64-linux-gnu libffi8:arm64 libedit2:arm64 libzstd1:arm64 libxml2:arm64
 
 target_triple="aarch64-linux-gnu" $compiler c -cache -static -stdpath $dir/../src -i $dir/../src -out $out_dir $dir/../src/std
 
@@ -18,36 +28,24 @@ if [ ! "$?" -eq "0" ]; then
   exit 1
 fi
 #todo use toolchain's std dir?
-linker=$($dir/find_llvm.sh clang)
-target_triple="aarch64-linux-gnu" LD=$linker $compiler c -norun -cache -stdpath $dir/../src -i $dir../src -out $out_dir -flags "$flags" -name $name $dir/../src/parser
+#linker=$($dir/find_llvm.sh clang)
+linker="aarch64-linux-gnu-g++-13"
+target_triple="aarch64-linux-gnu" LD=$linker $compiler c -norun -nolink -cache -stdpath $dir/../src -i $dir../src -out $out_dir -flags "$flags" -name $name $dir/../src/parser
 
 if [ ! "$?" -eq "0" ]; then
   echo "error while compiling"
   exit 1
 fi
 
-suffix=$(./find_llvm.sh)
 
 objs=""
 #compile .ll to .o
-for file in $out_dir/*.ll; do
-  echo "compiling $file"
-  if [ "$2" == "-pic" ]; then
-    llc$suffix -filetype=obj --relocation-model=pic $file
-  else
-    llc$suffix -filetype=obj $file
-  fi
-
-  if [ ! "$?" -eq "0" ]; then
-    echo "Compilation failed for $file"
-    exit 1
-  fi
-  nm="${file%.*}"
-  objs="$objs ${nm}.o"
+for file in $out_dir/*.o; do
+  objs="$objs $file"
 done
 
 #link .o files
-cmd="clang++$suffix -lstdc++ -o $name $objs ../cpp_bridge/build/libbridge.a $libdir/libLLVM.so"
+cmd="$linker -lstdc++ -o $name $objs $toolchain_target/lib/libbridge.a $toolchain_target/lib/libLLVM.so.19.1"
 echo "$cmd"
 $cmd
 
