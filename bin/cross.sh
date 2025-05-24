df2df1420ee26f86d8c6d9d09de78031877672d8
Mesut <mesutdogansoy@gmail.com>
2025-05-24 16:08:42 +0300

    arm64

--- bin/cross.sh
+++ bin/cross.sh
@@ -1,11 +1,5 @@
 dir=$(dirname $0)
 
-echo "$1,$2"
-pwd
-ls
-ls $1
-ls $2
-
 if [ -z "$1" ]; then
  echo "provide host toolchain dir"
  exit
@@ -23,9 +17,13 @@ build=$dir/../build
 name="x_arm64"
 out_dir=$build/${name}_out
 
-sudo dpkg --add-architecture arm64
-sudo apt update
-sudo apt install -y g++--aarch64-linux-gnu libffi8:arm64 libedit2:arm64 libzstd1:arm64 libxml2:arm64
+sudo=""
+if command -v sudo 2>&1 >/dev/null; then
+  sudo="sudo"
+fi
+$sudo dpkg --add-architecture arm64
+$sudo apt update
+$sudo apt install -y g++--aarch64-linux-gnu libffi8:arm64 libedit2:arm64 libzstd1:arm64 libxml2:arm64
 
 target_triple="aarch64-linux-gnu" $compiler c -cache -static -stdpath $dir/../src -i $dir/../src -out $out_dir $dir/../src/std
 
