1c6e7f6a748d3723802479adfb302ed9e87d9f9a
mesut <mesutdogansoy@gmail.com>
2025-05-29 08:45:51 +0000

    f

--- bin/docker.sh
+++ bin/docker.sh
@@ -5,4 +5,6 @@ if [ -z "$1" ]; then
  exit
 fi
 docker build -t cross -f ./bin/Dockerfile-cross .
+docker create --name cross cross
 docker cp cross:/home/lang/x-toolchain-$version-aarch64.zip .
+docker rm -f cross
