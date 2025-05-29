5418ee5728f97d54e315cd67eb0a43d31fd0a5fe
mesut <mesutdogansoy@gmail.com>
2025-05-29 09:53:38 +0000

    f

--- bin/docker.sh
+++ bin/docker.sh
@@ -4,6 +4,8 @@ if [ -z "$1" ]; then
  echo "provide version"
  exit
 fi
+version=$1
+
 docker build -t cross -f ./bin/Dockerfile-cross .
 docker create --name cross cross
 docker cp cross:/home/lang/x-toolchain-$version-aarch64.zip .
