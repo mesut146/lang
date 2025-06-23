26e4ab4d7d600a4033a56ec4264f25cc55f3f4d8
Mesut <mesutdogansoy@gmail.com>
2025-06-23 15:24:21 +0300

    j1

--- bin/build.sh
+++ bin/build.sh
@@ -36,7 +36,7 @@ flags="$flags $llvm_lib"
 flags="$flags -lstdc++"
 #todo use toolchain's std dir?
 linker=$($dir/find_llvm.sh clang)
-LD=$linker $compiler c -norun -cache -stdpath $dir/../src -i $dir/../src -out $out_dir -flags "$flags" -name $name $dir/../src/parser -j 1
+LD=$linker $compiler c -norun -cache -stdpath $dir/../src -i $dir/../src -out $out_dir -flags "$flags" -name $name $dir/../src/parser
 if [ ! "$?" -eq "0" ]; then
   echo "error while compiling"
   exit 1
